5ec08aa6879afdb942a0d517d513d2928440d68a
Nika1620 <niharikas443@gmail.com>
2023-05-04 01:47:16 +0530

    first commit

---
+++ form/book/src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppointmentForm from './AppointmentForm';
+const Footer = () => {
+  
+  return (
+    <div>
+      <main>
+      <h1>hello</h1>
+BCGVBVCV
+hjbgkhygbil
+uihy
+        <AppointmentForm/>
+      </main>
+    
+    </div>
+  );
+};
+
+export default Footer;
